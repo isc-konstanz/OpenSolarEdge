edb7fe4441443a1690beadcd40bbe2c07e592a73
Godwin Burkhardt <godwin.burkhardt@isc-konstanz.de>
2018-08-10 11:33:19 +0200

    Method submitRequest removed

--- projects/json-path-request/src/test/java/org/openmuc/jsonpath/TestHttpHandler.java
+++ projects/json-path-request/src/test/java/org/openmuc/jsonpath/TestHttpHandler.java
@@ -1,11 +1,8 @@
 package org.openmuc.jsonpath;
 
-import org.openmuc.jsonpath.HttpHandler;
-import org.openmuc.jsonpath.com.HttpGeneralException;
 import org.openmuc.jsonpath.data.Authentication;
 import org.openmuc.jsonpath.request.HttpCallable;
 import org.openmuc.jsonpath.request.HttpRequest;
-import org.openmuc.jsonpath.request.json.JsonResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,30 +20,7 @@ public class TestHttpHandler extends HttpHandler {
 		this.response = response;
 	}
 
-	protected synchronized JsonResponse submitRequest(HttpRequest request) throws HttpGeneralException {
-		if (logger.isTraceEnabled()) {
-			logger.trace("Requesting \"{}\"", request.toString());
-		}
-		long start = System.currentTimeMillis();
-		
-		HttpCallable task = createCallable(request);
-		JsonResponse response;
-		try {
-			response = task.call();
-			if (logger.isTraceEnabled()) {
-				String rsp = "Returned null";
-				if (response != null) {
-					rsp = response.getResponse();
-				}
-				logger.trace("Received response after {}ms: {}", System.currentTimeMillis() - start, rsp);
-			}
-			return response;		
-		} catch (Exception e) {
-			throw new HttpGeneralException("Aborted request \"" + request.toString() + "\": " + e);
-		}
-	}
-
-	
+	@Override
 	protected HttpCallable createCallable(HttpRequest request) {
 		TestHttpCallable task = new TestHttpCallable(request);
 		task.setResponse(response);
